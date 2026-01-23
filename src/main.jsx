e3c7d35c3ccb15cf1365213929626df84183c985
buyticketsmasterorg <buyticketsmaster.org@gmail.com>
2026-01-23 13:06:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UserApp from './UserApp.jsx';
+import AdminApp from './AdminApp.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// CHECK URL: If ?messenger=123 exists, load Admin. Otherwise, load User.
+const params = new URLSearchParams(window.location.search);
+const isMessenger = params.get('messenger') === '123' || params.get('messenger') === 'true';
+
+if (isMessenger) {
+  root.render(
+    <React.StrictMode>
+      <AdminApp />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <UserApp />
+    </React.StrictMode>
+  );
+}
 
 
